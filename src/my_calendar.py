192ef7f0f4df7099698b7aad26e01871dea21f1e
fang.liu <carol.liu@aihuishou.com>
2022-12-09 14:14:16 +0800

    2021-12-08

---
+++ src/my_calendar.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__time__ = "2022/12/9 14:11"
+__author__ = "fang.liu"
+
+import calendar
+
+# year = int(input("Enter the year of the required calendar "))
+# month = int(input("Enter the month of the required calendar "))
+year = 2022
+month = 12
+print(calendar.month(year, month))
